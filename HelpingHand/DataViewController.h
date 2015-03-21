2e0ca453caff58fea1bfe061a7a7d570ac94cc04
JennerPerry <jennerperry@jarthurcss-MacBook-Pro-59.local>
2015-03-21 13:35:28 -0700

    Initial Commit

---
+++ HelpingHand/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  HelpingHand
+//
+//  Created by JennerPerry on 3/21/15.
+//  Copyright (c) 2015 JennerFelton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
+
---
+++ HelpingHand/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  HelpingHand
+//
+//  Created by JennerPerry on 3/21/15.
+//  Copyright (c) 2015 JennerFelton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
